d060ce0acb5383a4c57ae3ebe216c8ad4075c5ba
Sean Tolbert <sean@seans-mbp.mynetworksettings.com>
2023-07-22 10:44:05 -0400

    adds priorities

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen gap-9">
-        <h1>Welcome to BlockParty.Life</h1>
+        <h1 className="text-3xl font-extrabold">Welcome to BlockParty.Life</h1>
 
         <p>
           So we got hammered last night and decided we would help people get
@@ -17,18 +12,19 @@ function App() {
           start)
         </p>
 
-        <p>here is what we think it should do:</p>
+        <div className="flex flex-col gap-3">
+          <p>so far, here is what we think it should do:</p>
+          <ul className="text-left">
+            <li>facilitate rentals for things like grills and tents</li>
+            <li>
+              create a dashboard/ forum for the planners and participants of the
+              blockparty to communicate and pool resources
+            </li>
+            <li>expedite the road closure process with the client's county</li>
+          </ul>
+        </div>
 
-        <ol className="">
-          <li>facilitate rentals for things like grills and tents</li>
-          <li>
-            create a dashboard/ forum for the planners and participants of the
-            blockparty to communicate and pool resources
-          </li>
-          <li>
-
-          </li>
-        </ol>
+        its gonna be super dope
       </div>
     </>
   );
